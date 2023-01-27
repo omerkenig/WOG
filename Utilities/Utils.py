3f396ae480264b862c5d93ca459ec24e9fe523dd
omerkenig <omerkenig@gmail.com>
2023-01-27 11:19:20 +0200

    fix some error

--- Utilities/Utils.py
+++ Utilities/Utils.py
@@ -1,7 +1,7 @@
 import time
 from os import system
 
-SCORES_FILE_NAME = "Scores.txt"
+SCORES_FILE_NAME = 'Score/Scores.txt'
 BAD_RETURN_CODE: dict[str, str] = {"2001": "Cannot install requirements!",
                                    '3001': "cannot open playerdata.dat (scores file). check file exists/corrupted",
                                    "4001": "Game module doesn't contain an appropriate about() function. cannot add "
@@ -28,4 +28,3 @@ def checkData(dataFromUser, limitDown, limitUp):
 def Screen_cleaner():
     time.sleep(0.7)
     system('cls')
-
